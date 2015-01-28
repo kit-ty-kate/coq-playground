2485633186b1987c6859213881874928143180fb
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2015-01-28 15:24:02 +0000

    Bi-implication everywhere

--- logic.v
+++ logic.v
@@ -1,7 +1,17 @@
 Theorem biconditional_elimination :
-  forall (p q : Prop), (p <-> q) -> ((p -> q) /\ (q -> p)).
+  forall (p q : Prop), (p <-> q) <-> ((p -> q) /\ (q -> p)).
 Proof.
-  intros p q H.
+  intros p q.
+  split.
+
+  intro H.
+  destruct H as [H G].
+  split.
+
+  apply H.
+  apply G.
+
+  intro H.
   destruct H as [H G].
   split.
 
@@ -10,20 +20,36 @@ Proof.
 Qed.
 
 Theorem non_contradiction :
-  forall (p : Prop), (p /\ ~p) -> False.
+  forall (p : Prop), (p /\ ~p) <-> False.
 Proof.
-  intros p H.
+  intro p.
+  split.
+
+  intro H.
   destruct H as [H G].
 
   absurd p.
   apply G.
   apply H.
+
+  intro H.
+  contradict H.
 Qed.
 
 Theorem commutativity_and :
-  forall (p q : Prop), p /\ q -> q /\ p.
+  forall (p q : Prop), p /\ q <-> q /\ p.
 Proof.
-  intros p q H.
+  intros p q.
+  split.
+
+  intro H.
+  destruct H as [H G].
+  split.
+
+  apply G.
+  apply H.
+
+  intro H.
   destruct H as [H G].
   split.
 
@@ -32,22 +58,37 @@ Proof.
 Qed.
 
 Theorem commutativity_or :
-  forall (p q : Prop), p \/ q -> q \/ p.
+  forall (p q : Prop), p \/ q <-> q \/ p.
 Proof.
-  intros p q H.
-  destruct H as [H | G].
+  intros p q.
+  split.
+
+  intro H.
+  destruct H as [H | H].
 
   right.
   apply H.
 
   left.
-  apply G.
+  apply H.
+
+  intro H.
+  destruct H as [H | H].
+
+  right.
+  apply H.
+
+  left.
+  apply H.
 Qed.
 
 Theorem associativity_and :
-  forall (p q r : Prop), p /\ (q /\ r) -> (p /\ q) /\ r.
+  forall (p q r : Prop), p /\ (q /\ r) <-> (p /\ q) /\ r.
 Proof.
-  intros p q r H.
+  intros p q r.
+  split.
+
+  intro H.
   destruct H as [H G].
   destruct G as [G I].
   split.
@@ -56,44 +97,91 @@ Proof.
   apply H.
   apply G.
   apply I.
+
+  intro H.
+  destruct H as [H G].
+  destruct H as [H I].
+
+  split.
+  apply H.
+
+  split.
+  apply I.
+
+  apply G.
 Qed.
 
 Theorem associativity_or :
-  forall (p q r : Prop), p \/ (q \/ r) -> (p \/ q) \/ r.
+  forall (p q r : Prop), p \/ (q \/ r) <-> (p \/ q) \/ r.
 Proof.
-  intros p q r H.
-  destruct H as [H | G].
+  intros p q r.
+  split.
+
+  intro H.
+  destruct H as [H | H].
 
   left.
   left.
   apply H.
 
-  destruct G as [G | I].
+  destruct H as [H | H].
   left.
   right.
-  apply G.
+  apply H.
 
   right.
-  apply I.
+  apply H.
+
+  intro H.
+  destruct H as [H | H].
+  destruct H as [H | H].
+
+  left.
+  apply H.
+
+  right.
+  left.
+  apply H.
+
+  right.
+  right.
+  apply H.
 Qed.
 
 Theorem idempotency_and :
-  forall (p : Prop), p /\ p -> p.
+  forall (p : Prop), p /\ p <-> p.
 Proof.
-  intros p H.
+  intro p.
+  split.
+
+  intro H.
   destruct H as [H _].
 
+  apply H.
+
+  intro H.
+  split.
+
+  apply H.
   apply H.
 Qed.
 
 Theorem idempotency_or :
-  forall (p : Prop), p \/ p -> p.
+  forall (p : Prop), p \/ p <-> p.
 Proof.
-  intros p H.
+  intro p.
+  split.
+
+  intro H.
   destruct H as [H | G].
 
   apply H.
   apply G.
+
+  intro H.
+  left.
+
+  apply H.
 Qed.
 
 Theorem absorption_and :
